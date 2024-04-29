66ff12c5d50dfee9086dbcbc39da0b0f6555feab
Inès Assouane <83213286+ines1102@users.noreply.github.com>
2024-04-29 15:48:40 +0000

    Code Kamelya

--- app/models.py
+++ app/models.py
@@ -1,27 +1,51 @@
-from app import db
+from app import app, db
+import hashlib
 
-# Définition du modèle Pro dans la base de données
-class Pro(db.Model):
-    _bind_key_ = 'pro'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(80), nullable=False)
-    mdp = db.Column(db.String(120), nullable=False)
+# Enveloppez le code qui nécessite le contexte de l'application Flask avec app.app_context()
+with app.app_context():
+    # Définition du modèle Pro dans la base de données
+    class Pro(db.Model):
+        _bind_key_ = 'users'
+        __tablename__ = 'pro'  # Nom de la table dans la base de données
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        email = db.Column(db.String(80), nullable=False)
+        mdp = db.Column(db.String(120), nullable=False)
+        is_connected = False  # Ajout de l'attribut is_connected pour suivre l'état de connexion
 
-    def get_id(self):
-        return str(self.id)
+        def get_id(self):
+            return str(self.id)
 
-    def is_active(self):
-        return True
+        def is_active(self):
+            return True
 
-# Définition du modèle Admin dans la base de données
-class Admin(db.Model):
-    _bind_key_ = 'admin'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(80), nullable=False)
-    mdp = db.Column(db.String(120), nullable=False)
+        def is_authenticated(self):
+            return self.is_connected  # Renvoie True uniquement si l'utilisateur est connecté
 
-    def get_id(self):
-        return str(self.id)
+        def is_anonymous(self):
+            return False
 
-    def is_active(self):
-        return True
+
+    # Définition du modèle Admin dans la base de données
+    class Admin(db.Model):
+        _bind_key_ = 'users'
+        __tablename__ = 'admin'  # Nom de la table dans la base de données
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        nom = db.Column(db.String(80), nullable=False)
+        mdp = db.Column(db.String(120), nullable=False)
+
+        def get_id(self):
+            return str(self.id)
+
+        def is_active(self):
+            return True
+    
+     # Vérifiez s'il y a déjà un administrateur
+    existing_admin = Admin.query.filter_by(nom='admin').first()
+
+    # Si aucun administrateur n'existe, créez-en un
+    if not existing_admin:
+        mdp="admin"
+        mdp_hache = hashlib.sha1(mdp.encode()).hexdigest()
+        admin = Admin(nom='admin', mdp=mdp_hache)
+        db.session.add(admin)
+        db.session.commit()
--- app/views.py
+++ app/views.py
@@ -2,9 +2,17 @@
 from flask import render_template, request, redirect, url_for, session, make_response 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, current_user, UserMixin, login_required
-from app import app, db
+from app import app, db, mail
 from app.models import Pro, Admin
+from mailbox import Message
+import random
+import string
 import hashlib
+from flask_mail import Message  
+
+with app.app_context():
+    # Appelez db.create_all() pour créer les tables dans la base de données
+    db.create_all()
 
 # Clé secrète pour la gestion des sessions
 app.secret_key = "123"
@@ -17,35 +25,51 @@ login_manager.login_view = 'connexion'
 def load_user(user_id):
     return Pro.query.get(int(user_id))
 
+
 # Route pour la page d'accueil
 @app.route('/')
 def index():
     return render_template('index.html', title='MDM')
 
-# Route pour l'inscription d'un médecin
+# Route pour la page d'inscription des professionnels
 @app.route('/inscription', methods=['GET', 'POST'])
 def inscription():
     error_message = None
     success_message = None
 
     if request.method == 'POST':
-        try:
-            email = request.form['email']
-            mdp = request.form['mdp']
-            mdp_hache = hashlib.sha1(mdp.encode()).hexdigest()
+        # Récupérer les données du formulaire
+        email = request.form['email']
+        mdp = request.form['mdp']
+        mdp_hache = hashlib.sha1(mdp.encode()).hexdigest()
 
-            pro_existant = Pro.query.filter_by(email=email).first()
-            if pro_existant:
-                error_message = "Un professionnel avec cet e-mail existe déjà. Veuillez vous connecter."
-            else:
-                nouveau_pro = Pro(email=email, mdp=mdp_hache)
-                db.session.add(nouveau_pro)
-                db.session.commit()
-                success_message = "Le professionnel a été inscrit avec succès."
-        except KeyError:
-            error_message = "Tous les champs doivent être remplis."
+        # Vérifier si un professionnel avec le même e-mail existe déjà
+        existing_pro = Pro.query.filter_by(email=email).first()
 
+        if existing_pro:
+            error_message = "Un professionnel avec cet e-mail existe déjà. Veuillez vous connecter."
+        else:
+            # Créer un nouveau professionnel
+            new_pro = Pro(email=email, mdp=mdp_hache)
+
+            # Ajouter le nouveau professionnel à la base de données
+            db.session.add(new_pro)
+            db.session.commit()
+
+            # Message de succès
+            success_message = "Le professionnel a été inscrit avec succès."
+
+    # Si la méthode est GET, afficher simplement le formulaire d'inscription
     return render_template('inscription.html', error_message=error_message, success_message=success_message)
+
+# Route pour envoyer un e-mail de test
+@app.route("/send_mail")
+def send_mail():
+    msg = Message("Subject", sender="ines.a1102@gmail.com", recipients=["ines.a1102@gmail.com"])
+    msg.body = "This is a test email"
+    mail.send(msg)
+    return "Mail has been sent"
+
 
 # Route pour la connexion d'un professionnel
 @app.route('/connexion', methods=['GET', 'POST'])
@@ -60,34 +84,40 @@ def connexion():
 
         if pro:
             if pro.mdp == mdp_hache:
+                pro.is_connected = True  # Définir is_connected sur True lorsque l'utilisateur est connecté
                 login_user(pro)
-                return redirect(url_for('dashboard'))
+                return render_template('dashboard.html')
             else:
                 error_message = "Mot de passe incorrect. Veuillez réessayer."
         else:
             error_message = "Email non trouvé. Vous n'êtes pas enregistré en tant que professionnel."
 
-    return render_template('connexion.html', error_message=error_message)
+    return render_template('index.html', error_message=error_message)
 
 
 # Route pour la connexion des administrateurs
 @app.route('/admin_access', methods=['GET', 'POST'])
 def admin_access():
     error_message = None
+    return render_template('admin_access.html', error_message=error_message)
+
+@app.route('/connexion_admin', methods=['GET', 'POST'])
+def connexion_admin():
+    error_message = None
 
     if request.method == 'POST':
-        email = request.form.get('email')
+        nom = request.form.get('nom')
         mdp = request.form.get('mdp')
 
-        if email is not None and mdp is not None:
+        if nom is not None and mdp is not None:
             mdp_hache = hashlib.sha1(mdp.encode()).hexdigest()
-            admin = Admin.query.filter_by(email=email).first()
+            admin = Admin.query.filter_by(nom=nom).first()
 
             if admin:
                 if admin.mdp == mdp_hache:
                     response = make_response(redirect(url_for('dashboard')))
                     response.set_cookie('admin_id', str(admin.id), secure=True, httponly=True, samesite='Strict')
-                    return response
+                    return render_template('inscription.html', error_message=error_message)
                 else:
                     error_message = "Mot de passe incorrect. Veuillez réessayer."
             else:
@@ -95,6 +125,7 @@ def admin_access():
         else:
             error_message = "Tous les champs doivent être remplis."
 
+    # Retourner le modèle avec le message d'erreur si la méthode est GET ou si la méthode est POST et qu'il y a une erreur
     return render_template('admin_access.html', error_message=error_message)
 
 
